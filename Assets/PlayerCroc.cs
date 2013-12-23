5cf18266981d2a642c5c8aafddf32ff60475d2a3
VHaugan <victor@haugan.dk>
2013-12-23 12:57:33 +0100

    physics changes
    changed jump variables and fixed a bug that prevented the player from
    jumping if he held the up key for too long.
    it is now possible to jump after you walk off a ledge - should look into
    fixing this

--- Assets/PlayerCroc.cs
+++ Assets/PlayerCroc.cs
@@ -13,25 +13,27 @@ public class PlayerCroc : MonoBehaviour
 		public KeyCode moveLeft;
 		public KeyCode moveRight;
 		private bool canJump = false;
+		private bool onGround = false;
 		private bool jumping = false;
 		private int jumpTime = 0;
-		private float speed = 7;
+		private float speed = 5;
 
 		// Update is called once per frame
 		void Update ()
 		{
 				if (Input.GetKey (moveUp)) {
 						if (canJump) {
-								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 10);
+								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 6);
+								onGround = false;
 								canJump = false;
 								jumping = true;
 						}
 						if (jumping) {
-								if (jumpTime == 12) {
+								if (jumpTime == 20) {
 										jumping = false;
 										jumpTime = 0;
 								}
-								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 15);
+								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 10);
 								jumpTime++;
 						}
 				} else {
@@ -46,13 +48,16 @@ public class PlayerCroc : MonoBehaviour
 						rigidbody2D.velocity = new Vector2 (speed * -1, rigidbody2D.velocity.y);
 				} else {
 						rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
-				}	
-		}
+				}
 
-		void OnCollisionEnter2D (Collision2D collision)
-		{
-		if (collision.gameObject.tag == "Ground" && !Input.GetKey (moveUp)) {
-						canJump = true;
+				if (onGround && !Input.GetKey (moveUp)) {
+					canJump = true;
+				}
+		}
+		
+		void OnCollisionEnter2D (Collision2D collision) {
+				if (collision.gameObject.tag == "Ground") {
+						onGround = true;
 				}
 		}
 }
