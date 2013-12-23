02de088c6cebe0058800c3c9adb9519a45805f22
Distant <phillip.oats@gmail.com>
2013-12-23 15:11:01 +0000

    final float

--- Assets/Gobbo.cs
+++ Assets/Gobbo.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class Gobbo : MonoBehaviour
 {
 	static int score = 0;
-
+	private float x = 0;
+	private float initHeight;
 		void Start ()
-		{
+	{initHeight = transform.position.y;
 		}
 		// Update is called once per frame
 		void Update ()
 		{
-
+		x++;
+		transform.position = new Vector2 (transform.position.x, initHeight + Mathf.Sin (x/30)/8);
 		}
 
 		void OnTriggerEnter2D (Collider2D collider)
--- Assets/bobScript.cs
+++ Assets/bobScript.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class bobScript : MonoBehaviour
 {
-		private float x = 0;
-	private float initHeight;
 		// Use this for initialization
 		void Start ()
 		{
-		initHeight = transform.position.y;
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				x++;
-		transform.position = new Vector2 (transform.position.x, initHeight + Mathf.Sin (x/20)/2);
+				
 		}
 }
