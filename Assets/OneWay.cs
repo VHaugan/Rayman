913cfa53d8de687e210fc99efe486db029a24efd
Distant <phillip.oats@gmail.com>
2014-01-02 20:16:40 +0000

    Update idk

---
+++ Assets/OneWay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Fist" && Fist.punching) {
+			if (rigidbody2D.IsSleeping ()) {
+				rigidbody2D.WakeUp ();
+				renderer.sortingOrder = 1;
+			}
+			else {
+				rigidbody2D.velocity = new Vector2 (Mathf.Sign (collider.rigidbody2D.velocity.x) * 5, 0);
+			}
+		}
+	}
+}
